86e7fe36aa3421bb5389e2287799eb186d2f2cca
Rahsaan Basek <rahsaanbasek@gmail.com>
2018-01-05 00:25:13 -0500

    phaser main.js update

--- 01-phaser/hello-world/js/main.js
+++ 01-phaser/hello-world/js/main.js
@@ -14,8 +14,12 @@ var GameState = {
 	preload: function(){
 		//load the image file "hello.png"
 		this.load.image('hello', 'assets/hello.png');
+		
 		//load file "background.png", give it a label of "mybackground"
 		this.load.image('mybackground', 'assets/background.png');
+		
+		//load file "character.png", give it a key of "player"
+		this.load.image('player', 'assets/character.png');
 	
 	},
 
@@ -26,6 +30,21 @@ var GameState = {
 		this.add.sprite(0, 0, 'hello');
 		//create a sprite, using the image with key "mybackground"
 		this.add.sprite(0, 0, 'mybackground');
+
+		var player = this.add.sprite(0, 0, 'player');
+
+		// set x to 100
+		player.x = 100;
+
+		// set y to 50
+		player.y = 50;
+
+		//set the position to (10, 20)
+		player.position = {x: 10, y: 20};
+
+		//set the position to a variable
+		var somewhere = {x: 1000, y: 1000};
+		player.position = somewhere;
 	}
 
 };
